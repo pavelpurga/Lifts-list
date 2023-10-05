7a6fa22c9b798c597cf9c5a98d914d77ec99a223
Pablo.ovrf <pashapetrokas@gmail.com>
2023-10-05 18:44:06 +0300

    making styles pretty

--- app/components/LiftModal.tsx
+++ app/components/LiftModal.tsx
@@ -22,41 +22,72 @@ const LiftModal = ({
                        handleSaveClick,
                    }: LiftModalProps) => {
     return (
-        <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", bgcolor: "background.paper", boxShadow: 24, p: 4, minWidth: 400 }}>
-            <h2>Edit Lift</h2>
+        <Box
+            sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                p: 4,
+                minWidth: 400,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
             <div>
-                <label>Name: {liftName} </label>
+                <h2>Name: {liftName} </h2>
             </div>
-            <div>
-                <div>Elevation Gain: {elevationGain}</div>
+            <div style={{marginTop:-30}}>
+                <h3>Elevation Gain: {elevationGain}</h3>
             </div>
-            <div>
+            <div style={{marginTop:20}}>
                 <label>Status:</label>
-                <Select value={liftStatus} onChange={(e) => setLiftStatus(e.target.value)}>
+                <Select sx={{height:20}} value={liftStatus} onChange={(e) => setLiftStatus(e.target.value)}>
                     <MenuItem value="OPEN">OPEN</MenuItem>
                     <MenuItem value="CLOSED">CLOSED</MenuItem>
                     <MenuItem value="HOLD">HOLD</MenuItem>
                 </Select>
             </div>
             <h3>Trail Access list:</h3>
-            <div style={{ maxHeight: 200, overflow: "auto", border: "1px solid black", marginTop: 10 }}>
+            <div
+                style={{
+                    border: "1px solid black",
+                    marginTop: 10,
+                    display: "flex",
+                    flexDirection: "column",
+                    maxHeight: 200,
+                    overflow: "auto",
+                }}
+            >
                 {selectedLift && (
-                    <ul>
+                    <Box>
                         {selectedLift.trailAccess.map((trail: any) => (
-                            <li key={trail.name}>
+                            <Box key={trail.name}
+                                 sx={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                margin: "10px",
+                                     padding: "10px",
+                                border: "1px solid black",
+                            }}>
                                 <ListItemText primary={`Name: ${trail.name}`} />
                                 <ListItemText primary={`Status: ${trail.status}`} />
-                            </li>
+                            </Box>
                         ))}
-                    </ul>
+                    </Box>
                 )}
             </div>
-            <Button variant="contained" onClick={handleCancelClick}>
-                Cancel
-            </Button>
-            <Button variant="contained" onClick={handleSaveClick}>
-                Save
-            </Button>
+            <div style={{paddingTop:20}}>
+                <Button variant="contained" onClick={handleCancelClick}>
+                    Cancel
+                </Button>
+                <Button sx={{marginLeft:18}} variant="contained" onClick={handleSaveClick}>
+                    Save
+                </Button>
+            </div>
         </Box>
     );
 };
